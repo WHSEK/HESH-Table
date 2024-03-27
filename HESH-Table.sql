672ca13b41c62627b2af98a619089d4b2faed59f
Egor Krylov <115998912+WHSEK@users.noreply.github.com>
2024-03-27 12:42:06 +0300

    Add files via upload

--- HESH-Table.sql
+++ HESH-Table.sql
@@ -58,9 +58,43 @@ INSERT INTO HESH_Table_ThirdPairs (HESHPair, DataFirst, DataSecond)
 
 SELECT * FROM HESH_Table_ThirdPairs
 
+SELECT SUM(HESHPair) AS TotalHESHPair
+FROM HESH_Table_ThirdPairs;
 
+CREATE TABLE HESH_Table_FourthPairs(
+HESHID INT IDENTITY NOT NULL PRIMARY KEY,
+HESHPair INT NOT NULL,
+DataFirst VARCHAR (100) NOT NULL,
+DataSecond VARCHAR (100) NOT NULL,
+);
 
+INSERT INTO HESH_Table_FourthPairs (HESHPair, DataFirst, DataSecond)
+	VALUES(12345, 'Alexey Ivanov', '123AB45'),
+	(67890, 'Sergey Petrov', '678CD90'),
+	(34567, 'Dmitry Sidorov', '345EF78'),
+	(90123, 'Anna Kuznetsova', '901GH23'),
+	(56789, 'Maria Smirnova', '567IJ10');
+
+SELECT * FROM HESH_Table_FourthPairs
+
+CREATE TABLE HESH_Table_FifthPairs(
+HESHID INT IDENTITY NOT NULL PRIMARY KEY,
+HESHPair INT NOT NULL,
+DataFirst VARCHAR (100) NOT NULL,
+DataSecond VARCHAR (100) NOT NULL,
+);
+
+INSERT INTO HESH_Table_FifthPairs (HESHPair, DataFirst, DataSecond)
+	VALUES(45678, 'Oleg Popov', '456KL09'),
+	(78901, 'Vladimir Putin', '789MN34'),
+	(13579, 'Elena Sidorova', '135PQ20'),
+	(24680, 'Igor Kuznetsov', '246ST56'),
+	(35791, 'Olga Petrova', '357UV72');
+
+SELECT * FROM HESH_Table_FifthPairs
 
 DROP TABLE HESH_Table_FirstPairs
 DROP TABLE HESH_Table_SecondPairs
 DROP TABLE HESH_Table_ThirdPairs
+DROP TABLE HESH_Table_FourthPairs
+DROP TABLE HESH_Table_FifthPairs
